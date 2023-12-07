2b325f43eae187d72216bb99db83648ee10df779
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-12-07 10:45:46 -0500

    feat: setup github workspace config

--- locals.tf
+++ locals.tf
@@ -12,7 +12,7 @@ locals {
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
     }
 
-    "fem-eci-terraform-gihub" = {
+    "fem-eci-terraform-github" = {
       description         = "Example automation workspace for GitHub resources"
       execution_mode      = "local"
       project_id          = module.project["fem-eci-project"].id
