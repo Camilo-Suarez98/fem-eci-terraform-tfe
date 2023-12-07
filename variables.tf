8e8c4a3932d8577dcec64f5d7970b7a8ddd0b445
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-12-07 09:38:52 -0500

    feat: setup vcs automation

--- variables.tf
+++ variables.tf
@@ -1,4 +1,14 @@
 variable "organization_name" {
-  default  = "my-personal-organization-project"
-  type = string
+  default = "my-personal-organization-project"
+  type    = string
+}
+
+variable "github_app_installation_id" {
+  default = 44862170
+  type    = number
+}
+
+variable "github_organization_name" {
+  default = "Camilo-Suarez98"
+  type    = string
 }
