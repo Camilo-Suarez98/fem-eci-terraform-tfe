d8ee2dd111b32a18faafec530eb23ad12d246e09
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-12-12 15:53:26 -0500

    feat: change in github repo

--- locals.tf
+++ locals.tf
@@ -14,14 +14,9 @@ locals {
 
     "fem-eci-github-tfe" = {
       description         = "Example automation workspace for second GitHub repo resources"
-      execution_mode      = "local"
+      execution_mode      = "remote"
       project_id          = module.project["fem-eci-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github-tfe"
     }
   }
-}
-
-moved {
-  from = module.workspace["fem-eci-workspace"]
-  to   = module.workspace["fem-eci-tfe"]
 }
